8043d58398d29b8c76140e361ae0b810bb03a418
jianglinChen191023 <1912623646@qq.com>
2021-11-20 20:21:14 +0800

    头部布局

--- src/plugins/element/index.js
+++ src/plugins/element/index.js
@@ -1,10 +1,14 @@
 /* element-ui 的样式 */
 import 'element-ui/lib/theme-chalk/index.css'
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Message } from 'element-ui'
+import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
 Vue.prototype.$message = Message
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,12 +1,31 @@
 <template>
   <div class="home">
-    <el-button type="info" @click="logout">退出</el-button>
+    <el-container class="home-container">
+      <!-- 头部区域 -->
+      <el-header class="el-header">
+        <div>
+          <img src="~assets/img/home/heima.png" alt="">
+          <span>电商后台管理系统</span>
+        </div>
+        <el-button type="info" @click="logout">退出</el-button>
+      </el-header>
+      <!--页面主体区域-->
+      <el-container>
+        <!-- 侧边栏 -->
+        <el-aside width="200px" class="el-aside">Aside</el-aside>
+        <!-- 内容主体 -->
+        <el-main class="el-main">Main</el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
+
 export default {
   name: 'Home',
+  components: {
+  },
   methods: {
     logout () {
       window.localStorage.clear()
@@ -16,6 +35,36 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="less" scoped>
+.home, .home-container {
+  height: 100%;
+}
+
+.el-header {
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+
+  > div {
+    display: flex;
+    align-items: center;
+
+    span {
+      margin-left: 15px;
+    }
+  }
+}
+
+.el-aside {
+  background-color: #333744;
+}
+
+.el-main {
+  background-color: #eaedf1;
+}
 
 </style>
