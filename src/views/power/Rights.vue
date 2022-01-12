1c77a5b32bca0e4ba66d680a8e15a185d297129c
jianglinChen191023 <1912623646@qq.com>
2022-01-13 02:40:51 +0800

    使用Tag标签

--- src/views/power/Rights.vue
+++ src/views/power/Rights.vue
@@ -14,7 +14,13 @@
         <el-table-column type="index"></el-table-column>
         <el-table-column label="权限名称" prop="authName"></el-table-column>
         <el-table-column label="路径" prop="path"></el-table-column>
-        <el-table-column label="权限等级" prop="level"></el-table-column>
+        <el-table-column label="权限等级" prop="level">
+          <template slot-scope="scope">
+            <el-tag v-if="scope.row.level === '0'">一级</el-tag>
+            <el-tag v-else-if="scope.row.level === '1'" type="success">二级</el-tag>
+            <el-tag v-else type="warning">三级</el-tag>
+          </template>
+        </el-table-column>
       </el-table>
     </el-card>
   </div>
