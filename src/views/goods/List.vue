fa9f386b31c4a9f2b55fadb8759025472a8ce5c3
jianglinChen191023 <1912623646@qq.com>
2022-03-07 10:49:00 +0800

    搜索和清空功能

--- src/views/goods/List.vue
+++ src/views/goods/List.vue
@@ -11,8 +11,8 @@
     <el-card>
       <el-row :gutter="20">
         <el-col :span="8">
-          <el-input placeholder="请输入内容" class="input-with-select">
-            <el-button slot="append" icon="el-icon-search"></el-button>
+          <el-input v-model="queryInfo.query" placeholder="请输入内容" class="input-with-select" clearable @clear="getGoodsList">
+            <el-button slot="append" icon="el-icon-search" @click="getGoodsList"></el-button>
           </el-input>
         </el-col>
         <el-col :span="4">
