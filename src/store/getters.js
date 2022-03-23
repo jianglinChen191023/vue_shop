97bf1896149ed018eb987dd222c28ec137e848c5
jianglinChen191023 <1912623646@qq.com>
2022-03-24 06:40:52 +0800

    CDN版本和开发匹配,和首页默认welcome

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,4 +1,4 @@
 export default {
   token: state => state.token || window.localStorage.getItem('token'),
-  activePath: state => state.activePath || window.localStorage.getItem('activePath')
+  activePath: state => state.activePath || window.localStorage.getItem('activePath') || 'welcome'
 }
